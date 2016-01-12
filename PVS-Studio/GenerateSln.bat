dcb74047e6bf431621925b34c5ecef1a78d8dfef
kalashnikov-viva64 <kalashnikov@viva64.com>
2016-01-12 16:01:40 +0200

    fix

--- PVS-Studio/GenerateSln.bat
+++ PVS-Studio/GenerateSln.bat
@@ -14,18 +14,19 @@ set with_symbols=
 set build_batch=generated_build_%build_version%-%build_number%-%build_mode%.bat
 
 cd /d "s:\src\env\MasterBuild\"
-call set-daletplus-dev-path.bat
   
 rem Platform selection
 if %Platform% EQU x86 goto lblx86
 if %Platform% EQU x64 goto lblx64
 goto lblError
 :lblx86
+  call set-daletplus-dev-path.bat
   call tclsh.exe generate_compilation_batch.tcl "s:\src" %build_state% %build_mode% Build ProjectList.txt "%build_batch%" DaletVersion.rc.tmpl "%build_version%" "%build_number%" "%with_symbols%"
   if %ERRORLEVEL% NEQ 0 goto lblError
   goto lblEndIf
 
 :lblx64
+  call set-daletplus-dev-path-x64.bat
   call tclsh.exe generate_compilation_batch_x64.tcl "s:\src" %build_state% %build_mode% Build ProjectList-x64.txt "%build_batch%" DaletVersion.rc.tmpl "%build_version%" "%build_number%" "%with_symbols%"
   if %ERRORLEVEL% NEQ 0 goto lblError
   goto lblEndIf
