67bac97b14d6c2f8697f8de839874c69acef1cde
kalashnikov-viva64 <kalashnikov@viva64.com>
2016-02-25 10:38:53 +0300

    Make PVS-Template

--- CC.Net/PrepareWorkspace.bat
+++ CC.Net/PrepareWorkspace.bat
@@ -1,33 +1,45 @@
-rem Usage: PrepareWorkspace.bat <PVS_Platform> <PVS_IncrediBuild>
-rem <PVS_Platform> - x86, x64
+rem Usage: PrepareWorkspace.bat <PVS_Platform> <PVS_IncrediBuild> <PVS_Team>
+rem <PVS_Platform> - x64, Amberfin_x86
 rem <PVS_IncrediBuild> - UseIB, NotUseIB
+rem <PVS_Team> - Dalet, Viva
 @echo on
 @setlocal
 pushd %~dp0
 cd /d %~dp0
 set PVS_Platform=%1
 set PVS_IncrediBuild=%2
+set PVS_Team=%3
 echo %TIME%: Starting PrepareWorkspace.bat
 
-if %PVS_Platform% EQU x86 goto lblx86
+rem Install last build of PVS-Studio
+if %PVS_Team% EQU Dalet call "c:\Program Files (x86)\PVS-Studio\PVS-Studio-Updater.exe" ^
+  /VERYSILENT /SUPPRESSMSGBOXES
+if %PVS_Team% EQU Viva call \\Viva64-build\Builder\PVS-Studio_setup.exe ^
+  /VERYSILENT /SUPPRESSMSGBOXES /COMPONENTS=Core,Standalone,MSVS,MSVS\2010,MSVS\2012
+if %ERRORLEVEL% NEQ 0 goto lblError
+
+if %PVS_Platform% EQU Amberfin_x86 goto lblAmberfin_x86
 if %PVS_Platform% EQU x64 goto lblx64
 goto lblError
-:lblx86
-  mkdir "C:\PVS Dalet logs x86 trunk"
-  mkdir "C:\PVS Dalet logs x86 trunk\temp"
-  mkdir "S:\src_suppress_x86_trunk"
-  robocopy "s:\src_suppress_x86_trunk" "s:\src" *.suppress /s /IS
-  call c:\PVS-Config\PVS-Studio\GenerateSln.bat x86
-  if %ERRORLEVEL% NEQ 0 goto lblError
-  call c:\PVS-Config\PVS-Studio\SlnSplitter.py ^
-    s:\src\env\MasterBuild\generated-all-projects-with-dependencies.sln 3
-  if %ERRORLEVEL% NEQ 0 goto lblError
-  if %PVS_IncrediBuild% EQU NotUseIB copy ^
-    /Y c:\PVS-Config\PVS-Studio\Settings-x86.xml %appdata%\PVS-Studio\Settings.xml
-  if %PVS_IncrediBuild% EQU UseIB copy ^
-    /Y c:\PVS-Config\IncrediBuild\Old-Dalet-Settings-x86.xml %appdata%\PVS-Studio\Settings.xml
-  goto lblEndIf
+:lblAmberfin_x86
+  rem Props
+  cd /d %appdata%\..\Local\Microsoft\MSBuild\v4.0\
+  copy /Y c:\PVS-Config\PVS-Studio\Amberfin-Microsoft.Cpp.x64.user.props Microsoft.Cpp.x64.user.props
+  copy /Y c:\PVS-Config\PVS-Studio\Amberfin-Microsoft.Cpp.Win32.user.props Microsoft.Cpp.Win32.user.props
+  goto lblAllOk
+  
 :lblx64
+  rem Props
+  cd /d %appdata%\..\Local\Microsoft\MSBuild\v4.0\
+  copy /Y c:\PVS-Config\PVS-Studio\Dalet-Microsoft.Cpp.x64.user.props Microsoft.Cpp.x64.user.props
+  copy /Y c:\PVS-Config\PVS-Studio\Dalet-Microsoft.Cpp.Win32.user.props Microsoft.Cpp.Win32.user.props
+
+  rem Settings
+  if %PVS_IncrediBuild% EQU NotUseIB copy ^
+    /Y c:\PVS-Config\PVS-Studio\Settings-x64.xml %appdata%\PVS-Studio\Settings.xml
+  if %PVS_IncrediBuild% EQU UseIB copy ^
+    /Y c:\PVS-Config\IncrediBuild\Old-Dalet-Settings.xml %appdata%\PVS-Studio\Settings.xml
+
   mkdir "C:\PVS Dalet logs x64 trunk"
   mkdir "C:\PVS Dalet logs x64 trunk\temp"
   mkdir "S:\src_suppress_x64_trunk"
@@ -37,13 +49,7 @@ goto lblError
   call c:\PVS-Config\PVS-Studio\SlnSplitter.py ^
     s:\src\env\MasterBuild\generated-x64-projects.sln 3
   if %ERRORLEVEL% NEQ 0 goto lblError
-  if %PVS_IncrediBuild% EQU NotUseIB copy ^
-    /Y c:\PVS-Config\PVS-Studio\Settings-x64.xml %appdata%\PVS-Studio\Settings.xml
-  if %PVS_IncrediBuild% EQU UseIB copy ^
-    /Y c:\PVS-Config\IncrediBuild\Old-Dalet-Settings.xml %appdata%\PVS-Studio\Settings.xml
-  goto lblEndIf
-:lblEndIf
-
+  
 :lblAllOk
 popd
 @endlocal
--- CC.Net/PublishingResults.bat
+++ CC.Net/PublishingResults.bat
@@ -1,12 +1,14 @@
-rem Usage: PublishingResults.bat <PVS_Platform> <PVS_Folder>
-rem <PVS_Platform> - x86, x64
+rem Usage: PublishingResults.bat <PVS_Platform> <PVS_Folder> <PVS_Team>
+rem <PVS_Platform> - x64, Amberfin_x86
 rem <PVS_Folder> - folder
+rem <PVS_Team> - Dalet, Viva
 @echo on
 @setlocal
 pushd %~dp0
 cd /d %~dp0
 set PVS_Platform=%1
 set PVS_Folder=%2
+set PVS_Team=%3
 set LastError=0
 echo %TIME%: Starting PublishingResults.bat
 
@@ -18,36 +20,21 @@ set M=%%k
 set Y=%%l
 ) 
 
-if %PVS_Platform% EQU Amberfin_x64 goto lblAmberfin_x64
-if %PVS_Platform% EQU x86 goto lblx86
-if %PVS_Platform% EQU x64 goto lblx64
+if %PVS_Platform% EQU Amberfin_x86 goto lblAmberfin_x86
+if %PVS_Team% EQU Dalet if %PVS_Platform% EQU x64 goto lblDaletx64
+if %PVS_Team% EQU Viva if %PVS_Platform% EQU x64 goto lblVivax64
 goto lblError
-:lblAmberfin_x64
-  set PlogFile=PVS_Amberfin_x64_WithSuppressedMessages.plog
+:lblAmberfin_x86
+  set PlogFile=PVS_Amberfin_x86_WithSuppressedMessages.plog
   cd /d %PVS_Folder%
   call C:\PVS-Config\PVS-Studio\PlogConverter.exe ^
-    -p %PlogFile% -a GA:1,2;64:1 --emailList="Emails_Amberfin.lst" --header="[auto] PVS-Studio Amberfin x64 Results" ^
+    -p %PlogFile% -a GA:1,2;64:1 --emailList="Emails_Amberfin.lst" --header="[auto] PVS-Studio Amberfin x86 Results" ^
 	--server=212.143.237.10 --port=26 --fromAddress=buildmaster@dalet.com --autoEmail
 	rem --sendEmail
   if %ERRORLEVEL% NEQ 0 set LastError=%ERRORLEVEL%  
   goto lblEndIf
   
-:lblx86
-  set PlogFile=generated-x86-projects.plog
-  if %WeekDay% EQU Sun set PlogFile=generated-x86-projects_WithSuppressedMessages.plog
-
-  rem PlogConverter
-  cd /d %PVS_Folder%
-  call C:\PVS-Config\PVS-Studio\PlogConverter.exe ^
-    -p %PlogFile% -a GA:1,2;64:1 --emailList="Emails.lst" --header="[auto] PVS-Studio Analysis x86 Results" ^
-	--server=212.143.237.10 --port=26 --fromAddress=buildmaster@dalet.com --autoEmail --sendEmail
-  if %ERRORLEVEL% NEQ 0 set LastError=%ERRORLEVEL%
-  
-  rem Update suppress bases
-  robocopy "S:\src" "S:\src_suppress_x86_trunk" *.suppress /s /IS
-  goto lblEndIf
-
-:lblx64
+:lblDaletx64
   set PlogFile=generated-x64-projects.plog
   if %WeekDay% EQU Sun set PlogFile=generated-x64-projects_WithSuppressedMessages.plog
   
@@ -56,6 +43,27 @@ goto lblError
   call C:\PVS-Config\PVS-Studio\PlogConverter.exe ^
     -p %PlogFile% -a GA:1,2;64:1 --emailList="Emails.lst" --header="[auto] PVS-Studio Analysis x64 Results" ^
 	--server=212.143.237.10 --port=26 --fromAddress=buildmaster@dalet.com --autoEmail --sendEmail
+  if %ERRORLEVEL% NEQ 0 set LastError=%ERRORLEVEL%
+  
+  rem Update suppress bases
+  robocopy "S:\src" "S:\src_suppress_x64_trunk" *.suppress /s /IS
+  goto lblEndIf
+  
+:lblVivax64
+  set PlogFile=generated-x64-projects.plog
+
+  rem PlogConverter
+  cd /d %PVS_Folder%
+  call C:\PVS-Config\PVS-Studio\PlogConverter.exe -p %PlogFile% -a GA:1,2;64:1
+  if %ERRORLEVEL% NEQ 0 set LastError=%ERRORLEVEL%
+  call c:\sendemail\sendemail.exe ^
+	-f builder@viva64.com ^
+	-t kalashnikov@viva64.com ^
+	-s smtp-9.1gb.ru ^
+	-o message-content-type=html message-file=generated-x64-projects.plog.html ^
+	-u [auto] PVS-Studio Analysis x64 Results ^
+	-o username=u410986 ^
+	-o password=213327374ui 
   if %ERRORLEVEL% NEQ 0 set LastError=%ERRORLEVEL%
   
   rem Update suppress bases
